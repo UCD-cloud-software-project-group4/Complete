db0bc8724c3ed6bc3b223ac7e1103b3a3bf16827
Andy <leduigs@users.noreply.github.com>
2014-11-14 13:13:51 +0000

    Update DataRedirection.java

--- Connections/DataRedirection.java
+++ Connections/DataRedirection.java
@@ -1,58 +1,94 @@
-package Hardware;
+/*This class contains utility methods which can be used to manipulate data by sending it to
+ * certain locations in various different ways */
+package Connections;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
 
-public class DataCenter implements Structures{
-	public List<Floor> dc_floors = new ArrayList<Floor>();
-	public int dc_id;
-	
-	public DataCenter() {
-		dc_id = 0;;		
-		dc_floors = null;
-	}
-	
-	public DataCenter(int dc_id, List<Floor> dc_floors) {
-		this.dc_id = dc_id;		
-		this.dc_floors = dc_floors;
-	}
-	
-	public void addFloor(Floor r){
-		dc_floors.add(r);
-	}
-	
-	public void removeFloor(Floor r){
-		dc_floors.remove(r);
-	}
-	
-	@Override
-	public double getMax(Object O) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public double getAverage() {
-		// TODO Auto-generated method stub
-		return 0;
+public class DataRedirection {
+	// clears a given directory with the address of that directory provided as a
+	// parameter
+	static void clearDirectory(String directory) {
+		String files;
+		File file = new File(directory);
+		File[] listOfFiles = file.listFiles(); // array to store all the files
+												// in a given directory
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				files = listOfFiles[i].getName();
+				{
+					boolean issuccess = new File(listOfFiles[i].toString())
+							.delete(); // delete each file
+				}
+			}
+		}
 	}
 
-	@Override
-	public int getID() {
-		// TODO Auto-generated method stub
-		return 0;
+	// clears a specific file
+	static void clearSpecificFile(String file) throws FileNotFoundException {
+		PrintStream console = System.out;
+		File file1 = new File(file);
+		FileOutputStream fos = new FileOutputStream(file1);
+		PrintStream ps = new PrintStream(fos);
+		System.setOut(ps);
+		System.out.println(""); // inserts a blank String into the directory to
+								// replace existing content
+		System.setOut(console);
 	}
 
-	@Override
-	public int setID() {
-		// TODO Auto-generated method stub
-		return 0;
+	// appends text (as a String) to a file. Each String appended goes to a new
+	// line in the file
+	static void appendTextToFile(String input, String location)
+			throws FileNotFoundException {
+		try {
+			PrintWriter out = new PrintWriter(new BufferedWriter(
+					new FileWriter(location, true)));
+			out.println(input);
+		} catch (IOException e) {
+		}
 	}
-	
-//	public String toString(){
-//		String value = "DataCenter ID: " + dc_id + " Floors: " + dc_floors.iterator().next().toString() + " Racks: " + dc_floors.iterator().next().showRacks();
-//		return value;
-//		
-//	}
 
+	// this sends text (as a String) to a file but each instance of it erases
+	// any previous content in the file
+	static void writeTextToFile(String input, String location) {
+		File file = new File(location);
+		String content = input;
+		try {
+			FileOutputStream fop = new FileOutputStream(file);
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			// get the content in bytes
+			byte[] contentInBytes = content.getBytes();
+			fop.write(contentInBytes);
+			fop.flush();
+			fop.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// parses the HTTP stream data into a human readable JSON format
+	static String httpToJSON(HttpURLConnection conn) throws IOException {
+		String result = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				conn.getInputStream()));
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = br.readLine()) != null) {
+			sb.append(line + "\n");
+			result = sb.toString();
+		}
+		return result;
+	}
 }
